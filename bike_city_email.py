eb963a0d3f724aae6733e5b7a6f0f77a1d685da3
Oskar Żak <oskarzak1999@gmail.com>
2023-01-10 00:58:35 +0100

    af

--- bike_city_email.py
+++ bike_city_email.py
@@ -8,23 +8,47 @@ import os
 save_file = open('citybike_dtf.csv', 'wt', encoding= 'utf-8')
 
 def main():
+    """_summary_:
+    The function allows you to find bike parks in individual countries and send their location to users by e-mail
 
+    Returns:
+        _type_: _description_
+    """
+
+    
 
     url = 'http://api.citybik.es/v2/networks'
     data = requests.get(url).json()
-    recipients = { "PL" : "oskarzak1999@gmail.com", "RU" : "patka379@onet.pl"  }
-
-    
-    class DailyDigestEmail:
+    recipients = { "PL" : "Recipient1@mail.com", "RU" : "Recipient2@mail.com"  }
+    for country in recipients:
+        size = (len(data['networks']))
+        list = []
+        number = 0
+        while number < size:
+            if country == data['networks'][number]['location']['country']:
+                list.append(data['networks'][number]['location'])
+            number +=1
+        
+        df_b = pd.DataFrame(list)
+        print(df_b)
+        print("operacja 1 zostala zakonczona")
+        class DailyDigestEmail:
+            """_summary_
+            The class determines the contents of the message, and all the data necessary to send it
+            """
             def __init__(self):
-                self.content = {"tabela": {"include": True, "content": str(df_b)}}
+                self.content = {"Table": {"include": True, "content": str(df_b)}}
 
-                self.recipients_list = [recipients[country]] #lista odbiorcow
+                self.recipients_list = [recipients[country]]
 
-                self.sender_credentials = {'email': 'oskarzak1999@outlook.com', # mail z ktorego bedzie wysylane
-                                        'password': 'Jestembogiem668!'} # haslo do maila
+                self.sender_credentials = {'email': 'Sender@mail.com',
+                                        'password': 'SenderPassword'}
 
             def send_email(self):
+                """_summary_
+                The function allows you to assign a message to a subject, recipient and sender,
+                it is also responsible for logging in to the server, and sending messages.
+                """
         
                 msg = EmailMessage()
                 msg['Subject'] = f'Bike parks in your area - {datetime.date.today().strftime("%d %b %Y")}'
@@ -45,24 +69,11 @@ def main():
             def format_message(self):
                 text = f'*~*~*~*~*~*~*~*~*~ Bike Parks In Your Area - {datetime.date.today().strftime("%d %b %Y")}*~*~*~*~*~*~*~*~*~\n\n'
 
-                if self.content["tabela"]["include"]:
+                if self.content["Table"]["include"]:
                     text+= 'You can find your parks here\n\n'
-                    text += f'"{self.content["tabela"]["content"]}"\n\n'
+                    text += f'"{self.content["Table"]["content"]}"\n\n'
 
                 return {'text': text}
-    for country in recipients:
-        size = (len(data['networks']))
-        list = []
-        number = 0
-        while number < size:
-            if country == data['networks'][number]['location']['country']:
-                list.append(data['networks'][number]['location'])
-            number +=1
-        
-        df_b = pd.DataFrame(list)
-        print(df_b)
-        print("operacja 1 zostala zakonczona")
-        
 
         if __name__ == "__main__":
             PASSWORD = os.environ.get('PASS')
